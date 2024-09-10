1990be8884beda64b742315c6936437f791aa050
rosaura10 <rosariosantamarina1@gmail.com>
2024-09-09 23:04:51 -0300

    Added dockerfile and configs

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -1,21 +1,24 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { DataSource } from 'typeorm';
 
 export const databaseProviders = [
   {
     provide: 'DATA_SOURCE',
-    useFactory: async () => {
+    imports: [ConfigModule],
+    useFactory: async (configService: ConfigService) => {
       const dataSource = new DataSource({
         type: 'postgres',
-        host: 'localhost',
-        port: 5432,
-        username: 'postgres',
-        password: 'postgres',
-        database: 'nutrillo-db',
+        host: configService.get<string>('POSTGRES_HOST'),
+        port: configService.get<number>('POSTGRES_PORT'),
+        username: configService.get<string>('POSTGRES_USER'),
+        password: configService.get<string>('POSTGRES_PASSWORD'),
+        database: configService.get<string>('POSTGRES_DB'),
         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
         synchronize: true,
       });
 
       return dataSource.initialize();
     },
+    inject: [ConfigService],
   },
 ];
